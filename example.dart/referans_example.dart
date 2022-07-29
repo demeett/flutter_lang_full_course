aa933cb9223def693ef825c28ec9273e2c6a2038
demeett <71386922+demeett@users.noreply.github.com>
2022-07-29 18:09:58 +0300

    referans tipler ve değer tipler düzenlenmiş hali

--- example.dart/referans_example.dart
+++ example.dart/referans_example.dart
@@ -5,18 +5,6 @@ void main(List<String> args) {
   control.ControlMethod(null, 30, 45);
   control = ArrayValue();
   control.ControlMethod(null, [2, 5, 58, 8], [7]);
-  //Neden array birbirinin aynısı olmuyor.
-  // var value2 = 30;
-  // control.value = value2;
-  // value2 = 50;
-  // print(value2);
-  // print(control.value);
-  // control = ArrayValue([]);
-  // List array2 = [0, 3, 5, 9, 8];
-  // control.value = array2;
-  // array2.add(51);
-  // print(array2);
-  // print(control.value);
 }
 
 abstract class IReferanceControl {
